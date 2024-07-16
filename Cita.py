67fe10764f7cac0a3d035d9983637deb6bc6af6c
Rocccko <168186426+Rocccko@users.noreply.github.com>
2024-07-16 17:24:01 +0200

    Update Cita.py

--- Cita.py
+++ Cita.py
@@ -10,7 +10,7 @@ driver.get('https://icp.administracionelectronica.gob.es/icpplus/index.html')
 
 
 datos = [{"passaporte": "TuPasaporte", "nombre": "TuNombre",
-          "nacimiento": 1111, "Pais": "Cuba"}]
+          "nacimiento": 1111, "Pais": "TuPais"}]
 
 
 try:
